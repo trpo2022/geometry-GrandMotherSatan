2c59539e3255e281e0a583efc9ed75de3203ccdc
Kirill Sudomoykin <kirillon1211@mail.ru>
2022-02-23 21:18:30 +0700

    Create struct

--- figure.h
+++ figure.h
@@ -4,6 +4,17 @@
 #include <string.h>
 #define _TRUE         1
 #define _FALSE        0
-#define _STRING_LENGTH 100
+#define _STRING_LENGTH 50
 #define _INVALID_VALUE -919939
 #define _IS_CIRCLE    11
+
+typedef struct
+{
+  float x;
+  float y;
+  float radius;
+  int flag_x;
+  int flag_y;
+  int flag_radius;
+} Circle;
+
