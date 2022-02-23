690e9342bf5d9deadf7d6256a310b5b6984b15b0
Kirill Sudomoykin <kirillon1211@mail.ru>
2022-02-23 21:42:27 +0700

    Add function main and func printf

---
+++ geometry.c
@@ -0,0 +1,14 @@
+#include "figure.h"
+
+int main()
+{
+  Circle circle;
+  char Figure[_STRING_LENGTH];
+  int flag_circle = 0;
+  int flag_staples = 0;
+
+  printf("Введите данные фигуры согласно образцу:\n");
+  printf("circle(0 0, 2.5)\n");
+  printf("Ввод: ");
+  fgets(Figure, _STRING_LENGTH, stdin);
+}
