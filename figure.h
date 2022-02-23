82e7f3083a693cfe01083c9eb20a5a66033cc519
Kirill Sudomoykin <kirillon1211@mail.ru>
2022-02-23 21:25:25 +0700

    Add function _if_is_cirecle

--- figure.h
+++ figure.h
@@ -6,7 +6,7 @@
 #define _FALSE        0
 #define _STRING_LENGTH 50
 #define _INVALID_VALUE -919939
-#define _IS_CIRCLE    11
+#define IS_CIRCLE    11
 
 typedef struct
 {
@@ -17,4 +17,15 @@ typedef struct
   int flag_y;
   int flag_radius;
 } Circle;
+
+int _if_is_circle(char *figure)
+{
+  if ((figure[0] == 'c' || figure[0] == 'C') &&
+      (figure[1] == 'i' || figure[1] == 'I') &&
+      (figure[2] == 'r' || figure[2] == 'R') &&
+      (figure[3] == 'c' || figure[3] == 'C') &&
+      (figure[4] == 'l' || figure[4] == 'L') &&
+      (figure[5] == 'e' || figure[5] == 'E'))
+  {
+    return IS_CIRCLE;
 
