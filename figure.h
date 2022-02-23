b49077ea63913780e69c364d3fc3f7438246015a
Kirill Sudomoykin <kirillon1211@mail.ru>
2022-02-23 21:06:31 +0700

    Add header files and macros

---
+++ figure.h
@@ -0,0 +1,9 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _TRUE         1
+#define _FALSE        0
+#define _STRING_LENGTH 100
+#define _INVALID_VALUE -919939
+#define _IS_CIRCLE    11
